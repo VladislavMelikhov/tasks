5989821c2e48ef5cc8bbdc14cf7571a4d04e68de
Vladislav Melikhov <VMelikhov@qulix.com>
2017-09-19 10:56:39 +0000

    Merged in show_list_of_items (pull request #2)
    Show list of items
    
    * first commit
    
    * refactoring
    
    * using recycler view
    
    * refactoring

---
+++ EmptyProject/app/src/main/java/com/example/melikhovva/firstapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.melikhovva.firstapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.LinearLayoutManager;
+
+import java.util.Arrays;
+
+public final class MainActivity extends Activity {
+
+    private static final int DATA_SIZE = 40;
+
+    @Override
+    protected void onCreate(final @Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final String[] data = new String[DATA_SIZE];
+        Arrays.fill(data, "");
+
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new StringsAdapter(data));
+    }
+}
